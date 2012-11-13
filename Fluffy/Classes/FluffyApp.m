2c642a5a2b32b6ef403af743b78e29d846d8f280
Mike Fullerton <mike@greentongue.com>
2012-11-13 12:30:01 -0800

    configs

---
+++ Fluffy/Classes/FluffyApp.m
@@ -0,0 +1,12 @@
+//
+//  FluffyApp.m
+//  Fluffy
+//
+//  Created by Mike Fullerton on 6/21/12.
+//  Copyright (c) 2012 GreenTongue Software. All rights reserved.
+//
+
+#import "FluffyApp.h"
+
+@implementation FluffyApp
+@end
