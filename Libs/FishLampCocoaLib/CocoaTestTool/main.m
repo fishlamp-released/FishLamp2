318b7d89d007d0f01a5920e079b455885579281a
Mike Fullerton <mike@greentongue.com>
2013-04-03 15:47:33 -0700

    framework fail

--- Libs/FishLampCocoaLib/CocoaTestTool/main.m
+++ Libs/FishLampCocoaLib/CocoaTestTool/main.m
@@ -7,9 +7,46 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "FLTestToolMain.h"
+
+#define kTestPath @"/Users/mike/Library/Application Support/com.fishlamp.tests"
+
+//extern int FLTestToolMain(int argc, const char *argv[]);
 
 int main(int argc, const char * argv[]) {
-    return FLTestToolMain(argc, argc);
+
+    @autoreleasepool {
+        @try {
+            NSError* error = nil;
+            NSArray* allItems = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:kTestPath error:&error];
+            if(error) {
+                NSLog([error description]);
+                return 1;
+            }
+            
+            for(NSString* path in allItems) {
+                NSBundle *framework = [[NSBundle alloc ] initWithPath:path];
+                if([framework preflightAndReturnError:&error]) {
+                    if([framework load]) {
+                        NSLog(@"Loaded: %@", [path lastPathComponent]);
+                        [framework unload];
+                    }
+                }
+                else {
+                    NSLog([error localizedDescription]);
+                }
+                FLRelease(framework);
+            }
+
+        
+            return 0;
+        }
+        @catch(NSException* ex) {
+            NSLog(@"uncaught exception: %@", [ex reason]);
+            return 1;
+        }
+        
+        return 0;
+    }
+    
 }
 
