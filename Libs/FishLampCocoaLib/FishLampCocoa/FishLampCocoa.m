b4b588b9ca4696b58bc6ef50d8c61309f572bde5
Mike Fullerton <mike@greentongue.com>
2013-04-03 16:19:59 -0700

    kill me now

---
+++ Libs/FishLampCocoaLib/FishLampCocoa/FishLampCocoa.m
@@ -0,0 +1,13 @@
+//
+//  FishLampCocoa.m
+//  FishLampCocoa
+//
+//  Created by Mike Fullerton on 4/3/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import "FishLampCocoa.h"
+
+@implementation FishLampCocoa
+
+@end
---
+++ Libs/FishLampCocoaLib/FishLampCocoaFramework/FishLampCocoa.h
@@ -0,0 +1,13 @@
+//
+//  FishLampCocoa.h
+//  FishLampCocoa
+//
+//  Created by Mike Fullerton on 4/3/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FishLampCocoa : NSObject
+
+@end
