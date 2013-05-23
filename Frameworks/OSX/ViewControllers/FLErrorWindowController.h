d21316f16652745bcfac560527f3135c551527cf
Mike Fullerton <mike@greentongue.com>
2013-05-22 17:46:06 -0700

    fix error window controller

--- Frameworks/OSX/ViewControllers/FLErrorWindowController.h
+++ Frameworks/OSX/ViewControllers/FLErrorWindowController.h
@@ -7,12 +7,13 @@
 //  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
 //
 
-#import <Cocoa/Cocoa.h>
+#import "FishLamp.h"
+#import "FLErrors.h"
 
 @interface FLErrorWindowController : NSWindowController {
 @private
     IBOutlet NSTextField* _titleField;
-    IBOutlet NSTextField* _explanationField;
+    IBOutlet NSTextView* _explanationField;
     IBOutlet __unsafe_unretained NSButton* _okButton;
     NSString* _title;
     NSString* _explanation;
@@ -22,4 +23,12 @@
 
 + (id) errorWindowController:(NSString*) title explanation:(NSString*) explanation;
 
+@end
+
+@interface NSViewController (FLErrorWindowController)
+- (void) showErrorAlert:(NSString*) title caption:(NSString*) caption error:(NSError*) error;
+@end
+
+@interface NSWindowController (FLErrorWindowController)
+- (void) showErrorAlert:(NSString*) title caption:(NSString*) caption error:(NSError*) error;
 @end
--- Frameworks/OSX/ViewControllers/FLErrorWindowController.m
+++ Frameworks/OSX/ViewControllers/FLErrorWindowController.m
@@ -7,7 +7,9 @@
 //  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
 //
 
+#import "NSWindowController+FLModalAdditions.h"
 #import "FLErrorWindowController.h"
+#import "NSError+FLTimeout.h"
 
 @interface FLErrorWindowController ()
 
@@ -42,8 +44,65 @@
     [super windowDidLoad];
     
     _titleField.stringValue = _title;
-    _explanationField.stringValue = _explanation;
+    [_explanationField.textStorage appendAttributedString:FLAutorelease([[NSAttributedString alloc] initWithString:_explanation]) ];
 }
 
+
+@end
+
+@implementation NSViewController (FLErrorWindowController)
+
+- (void) showErrorAlert:(NSString*) title caption:(NSString*) caption error:(NSError*) error {
+    if(![error isCancelError]) {
+   
+// TODO (MWF): double dispatch this back into the error?
+     
+        if([error isTimeoutError]) {
+            title = NSLocalizedString(@"An operation on the server timed out.", nil);
+            caption = NSLocalizedString(@"Please check your connection and try again.", nil);
+        }
+        
+        if(!title) {
+            title = NSLocalizedString(@"An error occurred", nil);
+        }
+        if(!caption) {
+            caption = error.localizedDescription;
+        }
+        
+        FLErrorWindowController* controller = [FLErrorWindowController errorWindowController:title explanation:caption];
+                       
+        [self showModalWindow:controller withDefaultButton:controller.okButton];
+    }
+
+}
+
+@end
+
+
+@implementation NSWindowController (FLErrorWindowController)
+
+- (void) showErrorAlert:(NSString*) title caption:(NSString*) caption error:(NSError*) error {
+    if(![error isCancelError]) {
+   
+// TODO (MWF): double dispatch this back into the error?
+     
+        if([error isTimeoutError]) {
+            title = NSLocalizedString(@"An operation on the server timed out.", nil);
+            caption = NSLocalizedString(@"Please check your connection and try again.", nil);
+        }
+        
+        if(!title) {
+            title = NSLocalizedString(@"An error occurred", nil);
+        }
+        if(!caption) {
+            caption = error.localizedDescription;
+        }
+        
+        FLErrorWindowController* controller = [FLErrorWindowController errorWindowController:title explanation:caption];
+                       
+        [self.window showModalWindow:controller withDefaultButton:controller.okButton];
+    }
+
+}
 
 @end
