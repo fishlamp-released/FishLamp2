f32b093cf813b7c27f6eec06c5cfe8b98c17af22
Mike Fullerton <mike@greentongue.com>
2013-06-05 09:23:32 -0700

    script changes

--- Scripts/Scripts/version-tag.sh
+++ Scripts/Scripts/version-tag.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 plist_file="$1"
+options="$2"
+
+function usage() {
+	echo "usage:"
+	echo " version-tag <plistfile> -[p][b]"
+	echo " -p = push"
+	echo " -b = bump"
+	echo " -bp = bump and push"
+	exit 1
+}
 
 if [ "$plist_file" == "" ]; then
-	echo "usage:"
-	echo "version-tag <plistfile>"
-	exit 1
+	usage
+fi
+
+if [[ "$options" == "-*" ]]; then 
+	usage
 fi
 
 if [ ! -f "$plist_file" ]; then
-	echo "can't find plist file $plist_file"
+	echo "##! can't find plist file $plist_file"
+	usage
 	exit 1
 fi
 
@@ -22,25 +35,22 @@ if [ "$status" != "" ]; then
 	exit 1;
 fi
 
-if [ `fishlamp-ynq \"Bump version?\"` == "yes" ]; then
-	echo "";
+if [[ "$options" == "*b*" ]]; then
 	version-bump "$plist_file" || { exit 1; }
 	build_version=`version-get "$plist_file"` || { exit 1; }
-	git add "$plist_file"
-	git commit -a -m "new version: $build_version"
-else 
-	echo "";
+	git add "$plist_file" || { exit 1; }
+	git commit -a -m "new version: $build_version" || { exit 1; }
 fi
 
 tag="v$build_version"
 git tag $tag || { echo "##! adding git tag failed"; exit 1; }
 echo "# added tag $tag ok"
 
-if [ `fishlamp-ynq Push Changes?` == "yes" ]; then
-	echo "";
+if [ "$push" == "-p" ]; then
 	git push --tags origin $branch || { echo "##! pushing tag failed"; exit 1; }
 else 
-	echo "";
+	echo "# don't forget to push:"
+	echo "git push --tags origin $branch";
 fi
 
 
