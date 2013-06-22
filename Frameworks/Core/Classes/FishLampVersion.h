b35a1a894d2ddf91929087f25c136be62383e5a6
Mike Fullerton <mike@greentongue.com>
2013-06-22 09:41:28 -0700

    Tagged version 3.0.0.30

--- Frameworks/Core/Classes/FishLampVersion.h
+++ Frameworks/Core/Classes/FishLampVersion.h
@@ -1,4 +1,4 @@
-// version 3.0.0.29 tagged Wed Jun  5 11:37:56 PDT 2013
+// version 3.0.0.30 tagged Sat Jun 22 09:41:28 PDT 2013
 #ifndef FishLampVersion
-#define FishLampVersion @"3.0.0.29"
+#define FishLampVersion @"3.0.0.30"
 #endif
