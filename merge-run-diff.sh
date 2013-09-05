9bbcefbd34120753fad951dc29b378d3c092eedc
Mike Fullerton <mfullerton@paypal.com>
2013-09-05 15:08:08 -0700

    integrate from Monolith

--- merge-run-diff.sh
+++ merge-run-diff.sh
@@ -1,11 +1,59 @@
 #!/bin/bash
 
-function diff() {
-    new="Pieces/$1"
-    old="OldPieces/Old-$1"
-    echo "# diffing $old into $new"
 
-    ksdiff --wait "$old/Classes" "$new/Classes"  || { exit 1; }
+function rundiff() {
+
+#     open -e $*
+
+    open diff $*
+    pid=`ps -ax | grep FileMerge | awk '{print $1}' | head -1`
+    nps=1
+    while [ $nps -ne 0 ]
+    do
+    nps=`ps ax | grep $pid | grep -v "grep" | wc -l`
+    done
+
+}
+
+function diff() {
+    new="Pieces/${1:4}"
+    old="OldPieces/$1"
+
+    if [ -d "$new" ]; then
+
+
+        src="" 
+        dest="" 
+
+        if [ -d "$old/Classes" ]; then
+            src="$old/Classes"
+            dest="$new/Classes"
+        else
+            src="$old"
+            dest="$new"
+        fi
+
+        echo "# diffing $src into $dest"
+
+        ksdiff --wait  "$src" "$dest"
+        sleep 1
+#         -merge "$dest" || { exit 1; }
+
+#         opendiff "$src" "$dest" -merge "$dest" || { exit 1; }
+# 
+# #         open diff $*
+#         pid=`ps -ax | grep FileMerge | awk '{print $1}' | head -1`
+#         nps=1
+#         while [ $nps -ne 0 ]
+#         do
+#         nps=`ps ax | grep $pid | grep -v "grep" | wc -l`
+#         done
+
+    
+    else
+        echo "$new"
+    fi
+
 }
 
 # diff FishLamp-Core
@@ -19,16 +67,10 @@ function diff() {
 # diff FishLamp-ModelObject/Classes $cocoa_module/ModelObject
 # diff FishLamp-Test/Classes $cocoa_module/Testing
 # diff FishLamp-CodeGenerator/Classes	monolith/Frameworks/CodeGenerator
-# 
 
-files=`find Pieces -depth 1 -name "FishLamp-*"`
+files=`find OldPieces -depth 1 -name "Old-FishLamp-*"`
 
 for file in $files; do
-        
     name=`basename "$file"`
-
-    
-
     diff "$name"
-
 done
